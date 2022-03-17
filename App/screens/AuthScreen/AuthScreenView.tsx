ec08d9c61e8630c9731e3f278499904cf85dda74
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-17 17:41:23 +0100

    march 17th

---
+++ App/screens/AuthScreen/AuthScreenView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function AuthScreenView(props) {
+    return (
+        <View>
+            <Text>Hello world</Text>
+        </View>
+    );
+}
+
+export default AuthScreenView;
