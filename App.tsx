6a1c2c3236446f7665f6a6c85437b38c66c4c84e
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-17 17:56:50 +0100

    march 17th

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AuthScreenContainer } from './App/screens';
+
+function App(props) {
+  return (
+    <AuthScreenContainer />
+  );
+}
+
+export default App;
--- App/screens/AuthScreen/AuthScreenView.tsx
+++ App/screens/AuthScreen/AuthScreenView.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 
 const AuthScreenView = () => {
     return (
         <View>
-
+            <Text>Hello world</Text>
         </View>
     );
 }
