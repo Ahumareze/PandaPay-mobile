5839589e5584e4b182cb94ec5a2992fa0122ed6b
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-18 22:47:30 +0100

    march 18th

--- App/screens/HomeScreen/components/Transactions/TransactionItem.tsx
+++ App/screens/HomeScreen/components/Transactions/TransactionItem.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { View, StyleSheet, Text, ImageBackground } from 'react-native';
 import { strokeColor } from '../../../../utils/colors';
 
+const img = require('../../../../assets/cats3.png');
+
 function TransactionItem(props) {
     return (
         <View style={styles.container}>
-
+            <View style={styles.v1}>
+                <ImageBackground source={img} />
+            </View>
+            <View style={styles.v2} />
+            <View style={styles.v3} />
         </View>
     );
 };
@@ -16,7 +22,24 @@ const styles = StyleSheet.create({
         width: '100%',
         borderWidth: 2,
         borderColor: strokeColor,
-        borderRadius: 10
+        borderRadius: 10,
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    v1: {
+        height: '100%',
+        width: 55,
+        backgroundColor: 'red'
+    },
+    v2: {
+        height: '100%',
+        flex: 1,
+        backgroundColor: 'cyan'
+    },
+    v3: {
+        height: '100%',
+        width: 50,
+        backgroundColor: 'blue'
     }
 })
 
