0fde9d9d840d6496c74438e6ee1a015d76ace69e
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-20 14:15:44 +0100

    march 20th

--- App/screens/ProfileScreen/ProfileScreenView.tsx
+++ App/screens/ProfileScreen/ProfileScreenView.tsx
@@ -4,6 +4,8 @@ import { Text, View, StyleSheet } from 'react-native';
 //Imported Components
 import ProfilePic from './components/ProfilePic';
 import {width, height} from '../../utils/dimension';
+import { black } from '../../utils/colors';
+import ProfileSettings from './components/ProfileSettings/profileSettings';
 
 const img = require('../../assets/cat2.png');
 
@@ -12,8 +14,10 @@ function ProfileScreenView(props) {
         <View style={styles.container}>
             <View style={styles.userProfileDetails}>
                 <ProfilePic img={img} />
-                <Text>Username</Text>
+                <Text style={styles.username}>Fvture_the_creatvr</Text>
+                <Text style={styles.email}>futurethecreator@gmail.com</Text>
             </View>
+            <ProfileSettings />
         </View>
     );
 };
@@ -27,6 +31,18 @@ const styles = StyleSheet.create({
     },
     userProfileDetails: {
         paddingTop: 30
+    },
+    username: {
+        fontSize: 23,
+        color: black,
+        textAlign: 'center',
+        paddingTop: 10
+    },
+    email: {
+        fontSize: 20,
+        color: black,
+        textAlign: 'center',
+        opacity: 0.7
     }
 })
 
---
+++ App/screens/ProfileScreen/components/ProfileSettings/ProfileSettings.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { black, red } from '../../../../utils/colors';
+
+function ProfileSettings(props) {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 200,
+        width: '100%',
+    }
+})
+
+export default ProfileSettings;
