eedd31b9bfdadf33906ed1fc9dbbbb4b997c1675
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-21 21:42:38 +0100

    march 21th

--- App/screens/TransferScreen/components/Transfer/Transfer.tsx
+++ App/screens/TransferScreen/components/Transfer/Transfer.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { red } from '../../../../utils/colors';
 import { height, width } from '../../../../utils/dimension';
+import UserDetails from './UserDetails';
 
 function Transfer(props) {
     return (
         <View style={styles.container}>
-            
+            <UserDetails />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        height: height,
-        backgroundColor: red
+        paddingTop: 30
     }
 })
 
--- App/screens/TransferScreen/components/Transfer/UserDetails.tsx
+++ App/screens/TransferScreen/components/Transfer/UserDetails.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { black } from '../../../../utils/colors';
+
+const img = require('../../../../assets/cat1.png');
 
 function UserDetails(props) {
     return (
-        <View>
-
+        <View style={styles.container}>
+            <ImageBackground source={img} style={styles.userPic}  />
+            <View style={styles.detailsContainer}>
+                <Text style={styles.username}>Username</Text>
+                <Text style={styles.email}>email@gmail.com</Text>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    userPic: {
+        height: 60,
+        width: 60,
+        borderRadius: 30,
+        overflow: 'hidden'
+    },
+    username: {
+        fontSize: 20,
+        color: black,
+    },
+    email: {
+        fontSize: 18,
+        color: black,
+        opacity: 0.6
+    },
+    detailsContainer: {
+        paddingLeft: 10
+    }
+})
 
 export default UserDetails;
