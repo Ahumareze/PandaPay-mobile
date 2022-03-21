c6059b3e0bf9bd266cd18aabd37b9edae8b5ce60
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-21 16:32:43 +0100

    march 21th

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 /**
  * @format
  */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+//Imported Components
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+//Imported reducers
+import reducer from './App/redux/reducer/reducer';
+
+//Redux configuration
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+    mainReducer: reducer
+});
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+)
+
+const appUseRedux = () => {
+    return(
+        <Provider store={store}>
+            <App />
+        </Provider>
+    )
+}
+
+
+AppRegistry.registerComponent(appName, () => appUseRedux);
