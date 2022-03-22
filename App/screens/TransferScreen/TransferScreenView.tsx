e1d88c91e8116df213c2da229c7d4681d8f2527a
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-22 01:45:33 +0100

    march 22th

--- App/screens/TransferScreen/TransferScreenView.tsx
+++ App/screens/TransferScreen/TransferScreenView.tsx
@@ -42,16 +42,12 @@ function TransferScreenView(props) {
 
             <QRCodeScanner 
                 onRead={r => read(r)}
-                // flashMode={RNCamera.Constants.FlashMode.torch}
-                topContent={
-                <Text style={styles.centerText}>
-                    Go to{' '}
-                    <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
-                    your computer and scan the QR code.
-                </Text>
+                bottomContent={
+                    <TouchableOpacity style={styles.buttonTouchable}>
+                        <Text style={styles.buttonText}>OK. Got it!</Text>
+                    </TouchableOpacity>
                 }
             />
-
         </View>
     );
 }
