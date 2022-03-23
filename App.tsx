ac96960799117f9f00a8d2a78b5b4dbf3ef805b6
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-23 04:34:27 +0100

    march 23rd

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { StatusBar, View } from 'react-native';
+
+//Imported utilities
+import { useSelector, useDispatch } from 'react-redux';
+import * as actions from './App/redux/actions';
+
+//Imported Screens
 import Navigator from './App/navigation';
+import {AuthScreenContainer} from './App/screens';
 
 function App(props) {
   return (
