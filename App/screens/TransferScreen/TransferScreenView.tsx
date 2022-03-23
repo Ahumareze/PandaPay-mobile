94735154850d5d492ac7b520ea0f02464c59a3ef
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-23 01:48:34 +0100

    march 23rd

--- App/screens/TransferScreen/TransferScreenView.tsx
+++ App/screens/TransferScreen/TransferScreenView.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { black, white } from '../../utils/colors';
@@ -7,7 +7,7 @@ import { black, white } from '../../utils/colors';
 import Main from './components/Main/Main';
 import Transfer from './components/Transfer/Transfer';
 
-import { Loader } from '../../components';
+import { Button, Loader } from '../../components';
 
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import { RNCamera } from 'react-native-camera';
@@ -17,6 +17,8 @@ import { height, width } from '../../utils/dimension';
 function TransferScreenView(props) {
     const reciever = useSelector((state: any) => state.mainReducer.reciever);
     const loading = useSelector((state: any) => state.mainReducer.loading);
+
+    const [data, setData] = useState();
 
     useEffect(() => {
         console.log(reciever)
@@ -31,32 +33,40 @@ function TransferScreenView(props) {
         )
     }
 
+    let qrView = (
+        <QRCodeScanner 
+            onRead={r => read(r)}
+            // flashMode={RNCamera.Constants.FlashMode.torch}
+            topContent={
+            <Text style={styles.centerText}>
+                Go to{' '}
+                <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+                your computer and scan the QR code.
+            </Text>
+            }
+            bottomContent={
+                <TouchableOpacity style={styles.buttonTouchable} onPress={() => setData(null)} >
+                    <Text style={styles.buttonText}>OK. Got it!</Text>
+                </TouchableOpacity>
+            }
+        />
+    )
+    if(data){
+        qrView = (
+            <Button onClick={() => setData(null)} title='restart' />
+        )
+    }
+
     const read = (e) => {
-        console.log(e + 'hello')
+        console.log(e);
+        setData(e)
     }
 
     return (
         <View style={styles.container}>
             {/* <Text style={styles.title}>New Transaction</Text>
             {!loading ? view : <Loader />} */}
-
-            <QRCodeScanner 
-                onRead={r => read(r)}
-                // flashMode={RNCamera.Constants.FlashMode.torch}
-                topContent={
-                <Text style={styles.centerText}>
-                    Go to{' '}
-                    <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
-                    your computer and scan the QR code.
-                </Text>
-                }
-                bottomContent={
-                    <TouchableOpacity style={styles.buttonTouchable}>
-                        <Text style={styles.buttonText}>OK. Got it!</Text>
-                    </TouchableOpacity>
-                }
-            />
-
+            {qrView}
         </View>
     );
 }
